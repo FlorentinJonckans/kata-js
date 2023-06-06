4deb8b16f8a8584d3baa53d4f2a91bd624035c8c
Jonckans Florentin <flo.j62@hotmail.fr>
2023-06-06 11:16:57 +0200

    add DNAtoRNA.js as kata codewars

---
+++ DNAtoRNA.js
@@ -0,0 +1,24 @@
+// Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+// Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+// Create a function which translates a given DNA string into RNA.
+
+// For example:
+
+// "GCAT"  =>  "GCAU"
+
+function DNAtoRNA(dna) {
+    let newDna = dna.split("");
+    // console.log(newDna);
+    for(let i = 0 ; i < newDna.length ; i++){
+      if ( newDna[i] === "T" ){
+        newDna[i] = 'U';
+      }
+    }
+    return newDna.toString().replaceAll(",","");
+}
+
+// function DNAtoRNA(dna) {
+//     return dna.replaceAll('T','U');
+// }
