2e191c9b59046c9bfa6bf65ddc9a7c21730675c1
Jonckans Florentin <flo.j62@hotmail.fr>
2023-06-09 10:49:17 +0200

    add an example in DNAStrand.js as kata codewars

--- kyu-7/DNAStrand.js
+++ kyu-7/DNAStrand.js
@@ -20,3 +20,9 @@ function DNAStrand(dna){
     };
     return dna.replace(/./ig, e => object[e]);
 }
+
+// let pairs = {'A':'T','T':'A','C':'G','G':'C'};
+
+// function DNAStrand(dna){
+//   return dna.split('').map(function(v){ return pairs[v] }).join('');
+// }
